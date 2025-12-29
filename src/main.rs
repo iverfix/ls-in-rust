ecf7fc3760295927ecc24c08d59f4d24789853df
iverfix <iver.andreas.ugelvik@hotmail.com>
2025-12-29 19:26:26 +0100

    Setup logic

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+// File system module
+//
+// File type, with correct typing
+//
+// Input argument parser
+//
+// Input -> Parsing -> Set config -> fetch relevant files and dirs -> Format output -> Display
+//
+// Need an interface to figure out the terminal width
+//
+// Outwards dependencies: Terminal "gui" interface (for fetching terminal sizes), file system interface
+
+fn main() {
+    println!("Hello, world!");
+}
